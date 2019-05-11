4c832f375935f771006b01e2895633b3b79a79a7
cleefsouza <a.cleef.souza@gmail.com>
2019-05-11 02:26:12 -0300

    error details

--- src/main/java/com/cleef/rest/app/repository/ItemRepository.java
+++ src/main/java/com/cleef/rest/app/repository/ItemRepository.java
@@ -1,11 +1,16 @@
 package com.cleef.rest.app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cleef.rest.app.model.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Long>{
 	
-	// recupera o item pelo id
+	// retrieve item by id
 	Item findById(long id);
+	
+	// list items by type
+	List<Item> findByType(long id);
 }
